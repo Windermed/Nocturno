0c054aae96a42823fe8183e61c531f2e19561bb6
Jacobb626 <76225335+Jacobb626@users.noreply.github.com>
2021-10-26 17:07:37 -0400

    remove more shit

--- Inventory.h
+++ Inventory.h
@@ -5,7 +5,7 @@
 #include "Util.h"
 #include <iostream>
 
-// code for the quickbars and inventory
+//Credit: Polaris
 struct AFortAsQuickBars
 {
 public:
@@ -58,93 +58,57 @@ public:
     class SDK::UMaterialInstanceDynamic* BuildPreviewMarkerMID;
 };
 
-// SDK variables for building 
 SDK::AFortQuickBars* QuickBars;
-SDK::FGuid* m_pgEditToolDef;
-SDK::FGuid* m_pgPickaxe;
+SDK::FGuid EditToolGuid;
+SDK::FGuid PickaxeGuid;
 SDK::FGuid WallGuid;
 SDK::FGuid FloorGuid;
 SDK::FGuid StairGuid;
 SDK::FGuid RoofGuid;
-SDK::UFortEditToolItemDefinition* m_pEditToolDef;
 SDK::UFortWeaponMeleeItemDefinition* PickaxeDef;
-SDK::UFortBuildingItemDefinition* m_pWallBuildDef;
-SDK::UFortBuildingItemDefinition* m_pFloorBuildDef;
-SDK::UFortBuildingItemDefinition* m_pStairBuildDef;
-SDK::UFortBuildingItemDefinition* m_pRoofBuildDef;
-SDK::ABuildingActor* m_pTrapC;
-bool bTrapDone = false;
-std::map<SDK::FGuid*, SDK::UFortWeaponItemDefinition*> m_mItems;
-std::map<SDK::FGuid*, SDK::UFortTrapItemDefinition*> m_mTraps;
-int iInventoryIteration = 0;
-SDK::TArray<class SDK::UFortWorldItem*>* pItemInsts;
-SDK::ABuildingPlayerPrimitivePreview* WallPreview;
-SDK::ABuildingPlayerPrimitivePreview* FloorPreview;
-SDK::ABuildingPlayerPrimitivePreview* StairPreview;
-SDK::ABuildingPlayerPrimitivePreview* RoofPreview;
-SDK::UStaticMesh* StaticWall;
-SDK::UStaticMesh* StaticFloor;
-SDK::UStaticMesh* StaticStair;
-SDK::UStaticMesh* StaticRoof;
-SDK::UBuildingEditModeMetadata_Wall* m_pMetadataWall;
-SDK::UBuildingEditModeMetadata_Roof* m_pMetadataRoof;
-SDK::UBuildingEditModeMetadata_Stair* m_pMetadataStair;
-SDK::UBuildingEditModeMetadata_Floor* m_pMetadataFloor;
-bool m_bHasCycledWall = false;
-bool m_bHasCycledFloor = false;
-bool m_bHasCycledStair = false;
-bool m_bHasCycledRoof = false;
-bool m_bHasCycledWallOnce = false;
-bool m_bHasCycledFloorOnce = false;
-bool m_bHasCycledStairOnce = false;
-bool m_bHasCycledRoofOnce = false;
-SDK::FString CurrentBuildingMat = TEXT("WOOD");
-int m_iCurrentBuildPiece;
-SDK::UClass* m_pLastBuildClassForWall;
-SDK::UClass* m_pLastBuildClassForFloor;
-SDK::UClass* m_pLastBuildClassForStair;
-SDK::UClass* m_pLastBuildClassForRoof;
+std::map<SDK::FGuid*, SDK::UFortWeaponItemDefinition*> Items;
+std::map<SDK::FGuid*, SDK::UFortTrapItemDefinition*> Traps;
 SDK::AFortInventory* FortInventory;
 
 namespace Inventory {
     static inline void SetupInventory()
     {
-        m_pEditToolDef = SDK::UObject::FindObject<SDK::UFortEditToolItemDefinition>("FortEditToolItemDefinition EditTool.EditTool");
-        m_pWallBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Wall.BuildingItemData_Wall");
-        m_pFloorBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Floor.BuildingItemData_Floor");
-        m_pStairBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Stair_W.BuildingItemData_Stair_W");
-        m_pRoofBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_RoofS.BuildingItemData_RoofS");
+        auto EditToolDef = SDK::UObject::FindObject<SDK::UFortEditToolItemDefinition>("FortEditToolItemDefinition EditTool.EditTool");
+        auto WallBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Wall.BuildingItemData_Wall");
+        auto FloorBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Floor.BuildingItemData_Floor");
+        auto StairBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_Stair_W.BuildingItemData_Stair_W");
+        auto RoofBuildDef = SDK::UObject::FindObject<SDK::UFortBuildingItemDefinition>("FortBuildingItemDefinition BuildingItemData_RoofS.BuildingItemData_RoofS");
         auto pWood = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition WoodItemData.WoodItemData");
         auto pMetal = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition MetalItemData.MetalItemData");
         auto pStone = SDK::UObject::FindObject<SDK::UFortWeaponItemDefinition>("FortResourceItemDefinition StoneItemData.StoneItemData");
         auto pRockets = SDK::UObject::FindObject<SDK::UFortWeaponMeleeItemDefinition>("FortAmmoItemDefinition AmmoDataRockets.AmmoDataRockets");
-        m_mItems.insert_or_assign(Util::GenerateGuidPtr(), pWood);
-        m_mItems.insert_or_assign(Util::GenerateGuidPtr(), pMetal);
-        m_mItems.insert_or_assign(Util::GenerateGuidPtr(), pStone);
-        m_mItems.insert_or_assign(Util::GenerateGuidPtr(), pRockets);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pWood);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pMetal);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pStone);
+        Items.insert_or_assign(Util::GenerateGuidPtr(), pRockets);
 
         for (int i = 0; i < SDK::UObject::GetGlobalObjects().Num(); ++i)
         {
-            auto pObject = SDK::UObject::GetGlobalObjects().GetByIndex(i);
-            if (pObject != nullptr && pObject->GetFullName().find("FortniteGame") == std::string::npos)
+            auto object = SDK::UObject::GetGlobalObjects().GetByIndex(i);
+            if (object != nullptr && object->GetFullName().find("FortniteGame") == std::string::npos)
             {
-                if (pObject->GetFullName().rfind("FortAmmoItemDefinition", 0) == 0)
+                if (object->GetFullName().rfind("FortAmmoItemDefinition", 0) == 0)
                 {
-                    m_mItems[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(pObject);
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(object);
                 }
-                if (pObject->GetFullName().rfind("FortWeaponRangedItemDefinition", 0) == 0)
+                if (object->GetFullName().rfind("FortWeaponRangedItemDefinition", 0) == 0)
                 {
-                    m_mItems[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(pObject);
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponItemDefinition*>(object);
                 }
-                if (pObject->GetFullName().rfind("FortTrapItemDefinition", 0) == 0)
+                if (object->GetFullName().rfind("FortTrapItemDefinition", 0) == 0)
                 {
                     auto guid = Util::GenerateGuidPtr();
-                    m_mTraps[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(pObject);
-                    m_mItems[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(pObject);
+                    Traps[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(object);
+                    Items[guid] = reinterpret_cast<SDK::UFortTrapItemDefinition*>(object);
                 }
-                if (pObject->GetFullName().rfind("FortWeaponMeleeItemDefinition", 0) == 0)
+                if (object->GetFullName().rfind("FortWeaponMeleeItemDefinition", 0) == 0)
                 {
-                    m_mItems[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponMeleeItemDefinition*>(pObject);
+                    Items[Util::GenerateGuidPtr()] = reinterpret_cast<SDK::UFortWeaponMeleeItemDefinition*>(object);
                 }
             }
         }
@@ -152,7 +116,7 @@ namespace Inventory {
         auto FortController = reinterpret_cast<SDK::AFortPlayerController*>(Cores::PlayerController);
         FortInventory = reinterpret_cast<WorldInventory*>(Cores::PlayerController)->WorldInventory;
 
-        for (auto it = m_mItems.begin(); it != m_mItems.end(); it++)
+        for (auto it = Items.begin(); it != Items.end(); it++)
         {
             SDK::UFortItem* Item;
             SDK::UFortWorldItem* WorldItem;
@@ -183,28 +147,28 @@ namespace Inventory {
         FortInventory->Inventory.ItemInstances.Add(WorldPickaxeItem);
         QuickBars->ServerAddItemInternal(WorldPickaxeItem->GetItemGuid(), SDK::EFortQuickBars::Primary, 0);
 
-        auto WallBuildItem = m_pWallBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto WallBuildItem = WallBuildDef->CreateTemporaryItemInstanceBP(1, 0);
         auto WallWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(WallBuildItem);
         FortInventory->Inventory.ReplicatedEntries.Add(WallWorldBuildItem->ItemEntry);
         FortInventory->Inventory.ItemInstances.Add(WallWorldBuildItem);
         QuickBars->ServerAddItemInternal(WallWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 0);
         WallGuid = WallWorldBuildItem->GetItemGuid();
 
-        auto FloorBuildItem = m_pFloorBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto FloorBuildItem = FloorBuildDef->CreateTemporaryItemInstanceBP(1, 0);
         auto FloorWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(FloorBuildItem);
         FortInventory->Inventory.ReplicatedEntries.Add(FloorWorldBuildItem->ItemEntry);
         FortInventory->Inventory.ItemInstances.Add(FloorWorldBuildItem);
         QuickBars->ServerAddItemInternal(FloorWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 1);
         FloorGuid = FloorWorldBuildItem->GetItemGuid();
 
-        auto StairBuildItem = m_pStairBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto StairBuildItem = StairBuildDef->CreateTemporaryItemInstanceBP(1, 0);
         auto StairWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(StairBuildItem);
         FortInventory->Inventory.ReplicatedEntries.Add(StairWorldBuildItem->ItemEntry);
         FortInventory->Inventory.ItemInstances.Add(StairWorldBuildItem);
         QuickBars->ServerAddItemInternal(StairWorldBuildItem->GetItemGuid(), SDK::EFortQuickBars::Secondary, 2);
         StairGuid = StairBuildItem->GetItemGuid();
 
-        auto RoofBuildItem = m_pRoofBuildDef->CreateTemporaryItemInstanceBP(1, 0);
+        auto RoofBuildItem = RoofBuildDef->CreateTemporaryItemInstanceBP(1, 0);
         auto RoofWorldBuildItem = reinterpret_cast<SDK::UFortWorldItem*>(RoofBuildItem);
         FortInventory->Inventory.ReplicatedEntries.Add(RoofWorldBuildItem->ItemEntry);
         FortInventory->Inventory.ItemInstances.Add(RoofWorldBuildItem);
